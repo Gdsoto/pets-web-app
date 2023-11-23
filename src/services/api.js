43e47a6fd39fd1bbaf91b81c8d48e61c54281263
Gerson David <51761203+Gdsoto@users.noreply.github.com>
2023-11-23 13:23:24 -0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const PetsApi = axios.create({
-	baseURL: 'http://186.155.151.14:19994/api',
+	baseURL: 'http://planb-production.up.railway.app/api',
 });
 
 export default PetsApi;
